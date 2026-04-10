1b392c83ef8c55ccf6fe94755d2e0617c3e12fd8
badovb <vtbado@gmail.com>
2026-04-09 23:30:47 -0300

    fix frontend errors

---
+++ src/Login.js
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+function Login({ onLogin }) {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const logar = () => {
+    if (email === "admin@email.com" && senha === "123") {
+      onLogin();
+    } else {
+      alert("Login inválido");
+    }
+  };
+
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h2>Login do Sistema</h2>
+
+      <input
+        placeholder="Email"
+        value={email}
+        onChange={e => setEmail(e.target.value)}
+      />
+      <br /><br />
+
+      <input
+        type="password"
+        placeholder="Senha"
+        value={senha}
+        onChange={e => setSenha(e.target.value)}
+      />
+      <br /><br />
+
+      <button onClick={logar}>Entrar</button>
+    </div>
+  );
+}
+
+export default Login;
